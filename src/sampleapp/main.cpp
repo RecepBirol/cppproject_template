a7e3200ec554f9a7c5b0103ff6648361236ded41
Recep Birol Gul <recep.gul@siemens.com>
2023-05-27 20:35:47 +0000

    adds fmt

--- src/sampleapp/main.cpp
+++ src/sampleapp/main.cpp
@@ -1,9 +1,14 @@
+#include <fmt/format.h>
 #include <iostream>
 #include <samplelib/samplelib.hpp>
+#include <string>
 
 int main() {
     myproject::samplelib::say_hello();
     auto res = myproject::samplelib::factorial(5);
     std::cout << "factorial of " << 5 << " is " << res << '\n';
+    std::string name{};
+    std::cin >> name;
+    std::cout << fmt::format("Hello {}\n", name);
     return EXIT_SUCCESS;
 }
